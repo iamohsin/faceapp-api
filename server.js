33ef7a716fef01f0002d9337b013cbb529e053b0
iamohsin <59395132+iamohsin@users.noreply.github.com>
2020-02-13 00:18:15 +0500

    up

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const signin=require('./controllers/signin')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '4455',
-    database : 'faceapp'
+    connectionString: process.env.DATABASE_URL,
+    ssl:true
     }
   });
  
