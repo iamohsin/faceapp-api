9bd55d62c0155c761f351129f8e99ca602a6badb
iamohsin <59395132+iamohsin@users.noreply.github.com>
2020-02-12 19:55:34 +0500

    uploading

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/profile/:id',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-    console.log('app is running on port 3000');
+app.listen( process.env.PORT ||3000,()=>{
+    console.log(`app is running on port ${process.env.PORT}`);
     
 })
